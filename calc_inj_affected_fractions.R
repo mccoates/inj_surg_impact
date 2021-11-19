979073b678ae6a0fb7948ef3e1e07ef5b34cb7ab
mmcoates <coates.mm@gmail.com>
2021-11-18 21:19:45 -0800

    initial work

---
+++ calc_inj_affected_fractions.R
@@ -0,0 +1,60 @@
+## Matthew Coates
+## Make some affected fractions for injuries
+
+rm(list=ls())
+library(data.table)
+library(openxlsx)
+library(writexl)
+
+
+d <- data.table(read.xlsx("C:/Users/MattC/Documents/repos/inj_matrix/case_fatality.xlsx"))
+d <- d[,c("rei_level_1","rei_level_2","rei_level","rei_name","Case.Fatality.Assumption","DCP4.interv1","DCP4.interv2"),with=F]
+d <- d[!is.na(rei_name)]
+setnames(d,c("Case.Fatality.Assumption"),c("cf"))
+
+## get inj inc by cause
+inc <- data.table(read.xlsx("C:/Users/MattC/Documents/repos/inj_matrix/inj_inc_stupid_download.xlsx",startRow = 1,colNames=F))
+inc1 <- t(inc[1:10])
+for (i in 2:(nrow(inc)/10)) {
+  inc1 <- rbind(inc1,t(inc[(i*10+1):(i*10+10)]))
+}
+inc1 <- data.table(as.data.frame(inc1))
+setnames(inc1,c("measure_name","location_name","sex","age_group_name","cause_name","metric_name","year","tot_inc","upper","lower"))
+inc1 <- inc1[!is.na(measure_name)]
+inc1[,tot_inc:=as.numeric(as.character(tot_inc))]
+
+## merge onto causes
+gmat <- fread("C:/Users/MattC/Documents/repos/inj_matrix/en_matrix_global_incidence.csv")
+gmat <- merge(gmat,inc1[,c("cause_name","tot_inc"),with=F],by=c("cause_name"),all.x=T)
+gmat[,Pct_of_Ecode_Resulting_in_Ncode:=Incidence/tot_inc]
+d <- merge(d[,c("rei_name","cf","DCP4.interv1","DCP4.interv2"),with=F],gmat,by=c("rei_name"),all=T)
+d[,affected_fraction:=Pct_of_Ecode_Resulting_in_Ncode*cf]
+d[,num_e_end_n_death:=Incidence*cf]
+d <- melt(d,id.vars=c(names(d)[!names(d) %in% c("DCP4.interv1","DCP4.interv2")]),value.name="intervention")
+d[,variable:=NULL]
+d <- d[lowest_cause==1 & rei_level==2]
+d <- d[!is.na(intervention)]
+
+## get inj deaths by cause
+deaths <- data.table(read.xlsx("C:/Users/MattC/Documents/repos/inj_matrix/inj_deaths_stupid_download.xlsx",startRow = 1,colNames=F))
+deaths1 <- t(deaths[1:10])
+for (i in 2:(nrow(deaths)/10)) {
+  deaths1 <- rbind(deaths1,t(deaths[(i*10+1):(i*10+10)]))
+}
+deaths1 <- data.table(as.data.frame(deaths1))
+setnames(deaths1,c("measure_name","location_name","sex","age_group_name","cause_name","metric_name","year","tot_deaths","upper","lower"))
+deaths1 <- deaths1[!is.na(measure_name)]
+deaths1[,tot_deaths:=as.numeric(as.character(tot_deaths))]
+d <- merge(d,deaths1[,c("cause_name","tot_deaths"),with=F],by=c("cause_name"))
+d[,affected_fraction:=num_e_end_n_death/tot_deaths]
+
+d <- d[,list(affected_fraction=sum(affected_fraction)),by=c("intervention","cause_name","rei_name")]
+d2 <- copy(d[,list(affected_fraction=sum(affected_fraction)),by=c("intervention","cause_name")])
+
+write_xlsx(list(d,d2),"C:/Users/MattC/Documents/repos/inj_matrix/inj_affected_fractions.xlsx")
+
+
+
+
+
+
---
+++ en_matrix_etc.R
@@ -0,0 +1,57 @@
+## Matthew Coates
+## Code on injury external causes versus nature of injury
+
+rm(list=ls())
+library(data.table)
+library(openxlsx)
+
+datadir <- "C:/Users/MattC/Dropbox (Personal)/hms_dropbox_backup/BCEPS/inj/"
+
+## read in GBD 2019 injury hierarchy
+## this hierarchy is from FairChoices severity work--could be some errors, but will double check
+h <- readRDS("C:/Users/MattC/Documents/repos/inj_matrix/dws_table2019.RDS")
+## output just inj to paste into drive doc
+h <- h[order(hierarchy2019)]
+write.csv(h[level_1=="Injuries",c("hierarchy2019","level_1","level_2","level_3","level_4","level","lowest_cause",
+                                  "cause_name"),with=F],"C:/Users/MattC/Documents/repos/inj_matrix/inj_cause_hierarchy.csv",row.names=F)
+
+## n-code hierarchy just has a couple levels--extracting from GBD compare
+nh <- data.table(read.xlsx("C:/Users/MattC/Documents/repos/inj_matrix/n_code_hierarchy.xlsx"))
+
+
+fls <- dir(datadir,pattern="csv")
+
+d <- list()
+for (fl in fls) {
+  d[[fl]] <- fread(paste0(datadir,fl))
+}
+d <- rbindlist(d)
+
+if (length(unique(d$rei_name[!d$rei_name %in% nh$rei_name])) > 0) stop("issue")
+
+
+## first, save E-N matrix for global, all ages, both sexes (we can check validity of this later)
+en <- copy(d[measure_name=="Incidence" & location_name=="Global" & sex_name=="Both" & age_name=="All Ages" & metric_name=="Number"])
+en <- unique(en[,c("cause_name","rei_name","val"),with=F])
+setnames(en,"val","Incidence")
+en <- en[order(cause_name,rei_name)]
+en <- merge(en,h[,c("cause_name","hierarchy2019","level_1","level_2","level_3","level_4",
+                    "lowest_cause","level"),with=F],by=c("cause_name"),all.x=T)
+if (any(is.na(en$hierarchy2019))) stop("missing")
+en <- merge(en,nh,by=c("rei_name"),all.x=T)
+if (any(is.na(en$rei_level_1))) stop("missing")
+en <- en[order(hierarchy2019,rei_order)]
+en <- en[cause_name!="All causes"]
+en <- en[,c("level_1","level_2","level_3","level_4","level","lowest_cause","cause_name",
+            "rei_level_1","rei_level_2","rei_level","rei_name","Incidence"),with=F]
+en[,Pct_of_Ecode_Resulting_in_Ncode:=Incidence/sum(Incidence)*100,by=c("cause_name","rei_level")]
+en[,Pct_of_Ncode_from_lowest_Ecodes:=Incidence/sum(Incidence)*100,by=c("rei_name","lowest_cause")]
+en[lowest_cause==0,Pct_of_Ncode_from_lowest_Ecodes:=NA]
+
+test <- copy(en[,list(Pct_of_Ecode_Resulting_in_Ncode=sum(Pct_of_Ecode_Resulting_in_Ncode)),by=c("cause_name","rei_level")])
+test <- copy(en[lowest_cause==1,list(Pct_of_Ncode_from_lowest_Ecodes=sum(Pct_of_Ncode_from_lowest_Ecodes)),by=c("rei_name")])
+
+write.csv(en,"C:/Users/MattC/Documents/repos/inj_matrix/en_matrix_global_incidence.csv",row.names=F)
+
+
+
